a54b917619f0875259e5aa1b0a5b534f9fb127d8
AdrianBlakaj <ab42581@ubt-uni.net>
2020-02-19 13:39:02 +0100

    Added broken styling to the code and also backend validation

---
+++ server/src/routes.js
@@ -0,0 +1,11 @@
+const AuthenticationController = require('./controllers/AuthenticationController');
+
+const AuthenticationControllerPolicies = require('./policies/AuthenticationControllerPolicy');
+
+module.exports = app => {
+  app.post(
+    '/register',
+    AuthenticationControllerPolicies.register,
+    AuthenticationController.register
+  );
+};
